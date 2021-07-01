435756a3950aa9f6b451c5cf8fa2c9021c4c5182
gkirtirajprdxn <kirtirajg@prdxn.com>
2021-07-01 21:12:22 +0530

    added random posts to single-news page

--- wp-content/themes/mytheme/single-news.php
+++ wp-content/themes/mytheme/single-news.php
@@ -12,6 +12,23 @@ get_header();
   }
   ?>
   </article>
+
+  <div class="random-posts">
+    <h2>Random Posts</h2>
+    <?php
+      $args = array(
+          'post_type' => 'news',
+          'post_status' => 'publish',
+          'offset' => 1,
+          'posts_per_page' => 3
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post();
+          get_template_part('template-parts/content', 'archive');
+        endwhile;
+      endif; ?>
+  </div>
 	    
 <?php
 get_footer();
