e987404b03033c482a4e2bb7f0bebad23b914df6
gkirtirajprdxn <kirtirajg@prdxn.com>
2021-07-07 19:21:58 +0530

    refactor

--- wp-content/themes/mytheme/news.php
+++ wp-content/themes/mytheme/news.php
@@ -25,8 +25,7 @@ get_template_part('template-parts/content', 'select'); ?>
     $query = new WP_Query( $args );
     if ($query -> have_posts()) {
       while($query -> have_posts()) {
-        $query -> the_post(); 
-        // get_template_part('template-parts/content', 'select');
+        $query -> the_post();
         get_template_part('template-parts/content', 'archive');
       }
     } 
